5f48daf014227bf15aab8c68ee93427e72c5c7c4
sebastian <sleising@student.ethz.ch>
2020-03-21 22:22:43 +0100

    Data to df function

--- dashboard/plots.py
+++ dashboard/plots.py
@@ -2,15 +2,38 @@
 import plotly.express as px
 import pandas as pd
 
+col_profile = ['gender', 'isSmoker', 'testedPositiveOn', 'hasFlueVaccine', 'hasLungDisease', 'hasDiabetes', 'isObese', 'takeSteroids']
+col_symptoms = ['hasCough', 'hasFever', 'hasChills', 'feelsWeak',
+                'hasLimbPain', 'hasSniff', 'hasDiarrhea','hasSoreThroat', 'hasHeadache', 'hasBreathingProblem']
+
+def to_df(d):
+    """
+    Convert firebase data to pandas Dataframe. Each journal entry corresponds to one journal entry
+    :param d:
+    :return:
+    """
+    recs = []
+    for user, item in d.items():
+        journal = item['journal']
+        profile = item['profile']
+        for timestamp, entry in journal.items():
+            recs.append({"userid":user, "timestamp":timestamp, **profile, **entry})
+
+    df = pd.DataFrame.from_records(recs)
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
+    return df
+
 
 def dummy_function(data):
+    import pdb
+    pdb.set_trace()
     fig = px.scatter(x=range(10), y=range(10))
     return fig.to_html(full_html=False, include_plotlyjs=True)#, include_mathjax=True)
 
 
 def sun_burst(data):
 
-    colnames = ['corona','cough', 'fever','pain']
+    colnames = ['feelsWeak', 'hasSoreThroat', 'hasSniff', 'hasCough', 'hasHeadache', 'hasLimbPain', 'hasBreathingProblems', 'hasChills', 'hasFever', 'hasDiarrhea']
 
     empty_line = {}
     for key in colnames:
@@ -32,3 +55,4 @@ def sun_burst(data):
 
     fig = px.sunburst(frame, path = colnames)
     return fig.to_html(full_html=False, include_plotlyjs=True)
+
