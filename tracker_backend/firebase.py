65544d18dd0636411be9c53693d82591848bdfa0
heynd <heynd@student.ethz.ch>
2020-03-21 15:22:09 +0100

    get_data function

--- tracker_backend/firebase.py
+++ tracker_backend/firebase.py
@@ -1,16 +1,19 @@
-import firebase_admin
 from firebase_admin import credentials, firestore
+import firebase_admin
 
-# Depending on where you save your key, update this
-# TODO: store this safely
-PATH_TO_KEY = "../covid19journalapp-firebase-adminsdk-1ce2y-1b3571ca42.json"
+import os
 
-cred = credentials.Certificate(PATH_TO_KEY)
-firebase_admin.initialize_app(cred)
+def get_data():
+    key = eval(os.getenv("FIREBASE_CREDENTIALS"))
+    cred = credentials.Certificate(key)
+    firebase_admin.initialize_app(cred)
 
-db = firestore.client()
-users_ref = db.collection(u'users')
-docs = users_ref.stream()
+    db = firestore.client()
+    users_ref = db.collection(u'users')
+    docs = users_ref.stream()
 
-for doc in docs:
-    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+    docs_in_a_dict = dict()
+    for doc in docs:
+        docs_in_a_dict[doc.id] = doc.to_dict()
+
+    return docs_in_a_dict
