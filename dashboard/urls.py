3fae64c4dc172113123610c910e686439f2d58e9
sebastian <sleising@student.ethz.ch>
2020-03-21 14:55:24 +0100

    Add first rough scaffolding for plotting functions

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from dashboard import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.first_dashboard, name='index')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
 
+
 # Create your views here.
-def index(request):
-    return HttpResponse("This is where the dashboard will be displayed")
+from django.shortcuts import render
+
+
+def first_dashboard(request):
+    # get data from firebase
+    data = {}
+
+    # Plot functions that should be displayed in this dashboard
+    plot_functions = []
+
+    plot_list = []
+    for function in plot_functions:
+        try:
+            plot_list.append(plot_functions(data))
+        except Exception as e:
+            plot_list.append(f"<p>error in plotting: {e}<\p>")
+
+    context = {'plot_list':plot_list}
+
+    return render(request, 'dashboard/first_dashboard.html', context)
+
