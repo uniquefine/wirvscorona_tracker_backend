064852acec634a6fd3326f27e29dd337796b6d2a
sebastian <sleising@student.ethz.ch>
2020-03-21 16:12:56 +0100

    Remove Debug point

--- dashboard/plots.py
+++ dashboard/plots.py
@@ -2,7 +2,5 @@
 import plotly.express as px
 
 def dummy_function(data):
-    import pdb
-    pdb.set_trace()
     fig = px.scatter(x=range(10), y=range(10))
     return fig.to_html(full_html=False, include_plotlyjs=True)#, include_mathjax=True)
