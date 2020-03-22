c75066f3249071630bb25a2b113f8ba1af58fb74
Johannes Ladwig <johannesl@me.com>
2020-03-22 09:32:01 +0100

    Changed probs in generating data

--- dashboard/plots.py
+++ dashboard/plots.py
@@ -1,20 +1,40 @@
-
 import plotly.express as px
 import pandas as pd
-import datetime
+
+col_profile = ['gender', 'isSmoker', 'testedPositiveOn', 'hasFlueVaccine', 'hasLungDisease', 'hasDiabetes', 'isObese', 'takeSteroids']
+col_symptoms = ['hasCough', 'hasFever', 'hasChills', 'feelsWeak',
+                'hasLimbPain', 'hasSniff', 'hasDiarrhea','hasSoreThroat', 'hasHeadache', 'hasBreathingProblem']
+
+def to_df(d):
+    """
+    Convert firebase data to pandas Dataframe. Each journal entry corresponds to one journal entry
+    :param d:
+    :return:
+    """
+    recs = []
+    for user, item in d.items():
+        journal = item['journal']
+        profile = item['profile']
+        for timestamp, entry in journal.items():
+            recs.append({"userid":user, "timestamp":timestamp, **profile, **entry})
+
+    df = pd.DataFrame.from_records(recs)
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
+    return df
+
 
 def dummy_function(data):
+    import pdb
+    pdb.set_trace()
     fig = px.scatter(x=range(10), y=range(10))
     return fig.to_html(full_html=False, include_plotlyjs=True)#, include_mathjax=True)
 
 
 def sun_burst(data):
 
-    col_profile = ['gender', 'isSmoker', 'testedPositiveOn', 'hasFlueVaccine', 'hasLungDisease', 'hasDiabetes', 'isObese', 'takeSteroids']
-    col_symptoms = ['hasCough', 'hasFever', 'hasChills', 'feelsWeak',
-                    'hasLimbPain', 'hasSniff', 'hasDiarrhea','hasSoreThroat', 'hasHeadache', 'hasBreathingProblem']
+    colnames = ['feelsWeak', 'hasSoreThroat', 'hasSniff', 'hasCough', 'hasHeadache', 'hasLimbPain', 'hasBreathingProblems', 'hasChills', 'hasFever', 'hasDiarrhea']
+
     empty_line = {}
-    colnames = col_profile + col_symptoms
     for key in colnames:
         empty_line[key] = False
 
@@ -22,20 +42,15 @@ def sun_burst(data):
 
     for key, components in data.items():
         line = empty_line.copy()
-
+        line['corona'] = components['profile']['corona']
         for symptom, present in components['journal']['today'].items():
             if symptom in colnames:
                 line[symptom] = present
-        for condition, present in components['profile']
-            if condition in colnames:
-                line[condition] = present
-        frame = frame.append(line, ignore_index=True)
+            frame = frame.append(line, ignore_index=True)
 
-    frame['hasCorona'] = bool(frame['testedPositiveOn'])
-    for col in colnames[0:1,3:]:
+    for col in colnames:
         frame[col] = frame[col].replace(True, f'has{col}')
         frame[col] = frame[col].replace(False, f'no {col}')
 
     fig = px.sunburst(frame, path = colnames)
     return fig.to_html(full_html=False, include_plotlyjs=True)
-
--- dummydata/fake_data.py
+++ dummydata/fake_data.py
@@ -4,7 +4,7 @@ import json
 
 
 def rand_date():
-    random_date =time.time()-86400*random.choice([0,1,2,3,4,5,6])
+    random_date = time.time() - 86400 * random.choice([0, 1, 2, 3, 4, 5, 6])
     return random_date
 
 
@@ -16,12 +16,40 @@ def generate_data(nr_users, outpath):
                    'hasHeadache', 'hasBreathingProblem']
 
     data = {}
-    for user in range(nr_users):
-        data[user] = {'profile':{}, 'journal': {}}
-        data[user]['profile']['gender'] = random.choice(['Male', 'Female'])
+    probs = {
+        'corona': {'hasCough': 0.667, 'hasFever': 0.879, 'hasChills': 0.114,
+                   'feelsWeak': 0.381,
+                   'hasLimbPain': 0.148, 'hasSniff': 0.048,
+                   'hasDiarrhea': 0.037, 'hasSoreThroat': 0.139,
+                   'hasHeadache': 0.136, 'hasBreathingProblem': 0.186},
+        'flue': {'hasCough': 0.8, 'hasFever': 0.879, 'hasChills': 0.114,
+                 'feelsWeak': 0.8,
+                 'hasLimbPain': 0.8, 'hasSniff': 0.3, 'hasDiarrhea': 0.037,
+                 'hasSoreThroat': 0.139,
+                 'hasHeadache': 0.8, 'hasBreathingProblem': 0},
+        'sniff': {'hasCough': 0.1, 'hasFever': 0.1, 'hasChills': 0.114,
+                  'feelsWeak': 0.3,
+                  'hasLimbPain': 0, 'hasSniff': 1, 'hasDiarrhea': 0.02,
+                  'hasSoreThroat': 0.1,
+                  'hasHeadache': 0.3, 'hasBreathingProblem': 0},
+        'healthy': {'hasCough': 0, 'hasFever': 0, 'hasChills': 0,
+                    'feelsWeak': 0,
+                    'hasLimbPain': 0, 'hasSniff': 0, 'hasDiarrhea': 0,
+                    'hasSoreThroat': 0,
+                    'hasHeadache': 0.05, 'hasBreathingProblem': 0}}
 
-        if random.choice([True, False]):
-            data[user]['profile']['testedPositiveOn'] = rand_date()
+    for user in range(nr_users):
+        data[user] = {'profile': {}, 'journal': {}}
+        data[user]['profile']['gender'] = random.choice(['Male', 'Female'])
+        status = random.choices(['corona', 'flue', 'sniff', 'healthy'],
+                               weights=[0.1, 0.2, 0.3, 0.4])[0]
+
+        if status == 'corona' and random.choices([True, False],
+                                                weights=[0.8, 0.2])[0]:
+            data[user]['profile']['testedPositiveOn'] = random.choice(
+                [rand_date(), None])
+        else:
+            data[user]['profile']['testedPositiveOn'] = None
 
         for entry, value in zip(col_profile[2:],
                                 random.choices([True, False], k=6)):
@@ -30,16 +58,18 @@ def generate_data(nr_users, outpath):
                 data[user]['profile']['pregnant'] = False
 
         dates = []
-        for i in range(random.choice([1,2,3,4])):
-            dates += [time.time()-i*86400]
+        for i in range(random.choice([1, 2, 3, 4])):
+            dates += [time.time() - i * 86400]
 
         for date in dates:
             data[user]['journal'][date] = {}
-            for entry, value in zip(col_journal, random.choices([True, False],k=10)):
+            for entry in col_journal:
+                p_t = probs[status][entry]
+                value = random.choices([True, False], weights=[p_t, 1-p_t])[0]
                 data[user]['journal'][date][entry] = value
 
-        with open(outpath, 'w') as outfile:
-            json.dump(data, outfile, indent=1)
+    with open(outpath, 'w') as outfile:
+        json.dump(data, outfile, indent=1)
 
 
-generate_data(1000, 'sampledata.json')
+generate_data(1000, 'sampledata')
